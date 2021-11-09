5e063245c4f565bb70e8e985e67eed654e0043fb
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-11-09 15:01:54 +0100

    fix: use Cart static class in rendering cart data in CartServlet

--- src/main/java/com/codecool/shop/controller/CartServlet.java
+++ src/main/java/com/codecool/shop/controller/CartServlet.java
@@ -1,11 +1,11 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.service.ProductService;
+//import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,7 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.math.BigDecimal;
 
 @WebServlet(name = "hackerNewsServlet", urlPatterns = {"/cart"}, loadOnStartup = 1)
 public class CartServlet extends javax.servlet.http.HttpServlet {
@@ -25,13 +25,18 @@ public class CartServlet extends javax.servlet.http.HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
+        // adding products for testing purposes
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        Supplier lenovo = new Supplier("Lenovo", "Computers");
+        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+
+        Cart.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+        Cart.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
+        Cart.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("products", productService.getProductsForCategory(1));
+        context.setVariable("products", Cart.getAll());
         engine.process("product/cart.html", context, resp.getWriter());
     }
 }
