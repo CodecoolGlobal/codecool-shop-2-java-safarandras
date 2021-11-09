1040a21bd87c5275fe3ba151494a6359fa4dab6b
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-11-09 14:59:57 +0100

    fix: make Cart class static

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -1,4 +1,26 @@
 package com.codecool.shop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+
+    private static List<Product> items = new ArrayList<>();
+
+    public static void add(Product product) {
+        items.add(product);
+    }
+
+    public static Product find(int id) {
+        return items.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    public static void remove(int id) {
+        items.remove(find(id));
+    }
+
+    public static List<Product> getAll() {
+        return items;
+    }
+
 }
