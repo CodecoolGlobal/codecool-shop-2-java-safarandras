ea478d3eb0900e97fac1fcd348d211dd00a88713
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-11-09 11:32:17 +0100

    feat: add CartServlet with cart.html rendering

---
+++ src/main/java/com/codecool/shop/controller/CartServlet.java
@@ -0,0 +1,37 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.service.ProductService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "hackerNewsServlet", urlPatterns = {"/cart"}, loadOnStartup = 1)
+public class CartServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("products", productService.getProductsForCategory(1));
+        engine.process("product/cart.html", context, resp.getWriter());
+    }
+}
