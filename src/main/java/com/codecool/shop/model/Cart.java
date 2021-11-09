c4bbc1e7f8492b6ccae5cb4f94a1f4ba5f68abab
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-11-10 00:18:45 +0100

    feat: reimplement Cart class

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -1,26 +1,42 @@
 package com.codecool.shop.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Cart {
 
-    private static List<Product> items = new ArrayList<>();
+    private static HashSet<LineItem> lineItems = new HashSet<>();
 
     public static void add(Product product) {
-        items.add(product);
+        LineItem newItem = new LineItem(product);
+        if (!lineItems.isEmpty()) {
+            for (LineItem lineItem: lineItems) {
+                if (Objects.equals(product.name, lineItem.getProduct().getName())) {
+                    lineItem.increaseQuantityAndSubtotal();
+                } else {
+                    newItem.setId(lineItems.size() + 1);
+                    lineItems.add(newItem);
+                }
+            }
+        } else {
+            newItem.setId(1);
+            lineItems.add(newItem);
+        }
+
     }
 
-    public static Product find(int id) {
-        return items.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    public static LineItem find(String name) {
+        return lineItems.stream()
+                .filter(t -> t.getName().equals(name))
+                .findFirst()
+                .orElse(null);
     }
 
-    public static void remove(int id) {
-        items.remove(find(id));
+    public static void remove(String name) {
+        lineItems.remove(find(name));
     }
 
-    public static List<Product> getAll() {
-        return items;
+    public static HashSet<LineItem> getAll() {
+        return lineItems;
     }
 
 }
