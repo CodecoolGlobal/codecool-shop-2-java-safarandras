367b36da878f22b3de1cf1e1f8cb4f7fda42edef
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-11-10 00:36:59 +0100

    feat: add products and total rendering to CartServlet class

--- src/main/java/com/codecool/shop/controller/CartServlet.java
+++ src/main/java/com/codecool/shop/controller/CartServlet.java
@@ -34,9 +34,11 @@ public class CartServlet extends javax.servlet.http.HttpServlet {
         Cart.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         Cart.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
 
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("products", Cart.getAll());
+        context.setVariable("total", Cart.calculateTotalPrice());
         engine.process("product/cart.html", context, resp.getWriter());
     }
 }
