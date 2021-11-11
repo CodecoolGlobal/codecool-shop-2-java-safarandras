91dbfe15d3a243da343e3cf0e4c85a565a5531f1
Gezow <kokenyesdanone@gmail.com>
2021-11-11 12:17:41 +0100

    add imports and new post method to CartServlet from AddToCartServlet.java

--- src/main/java/com/codecool/shop/controller/CartServlet.java
+++ src/main/java/com/codecool/shop/controller/CartServlet.java
@@ -2,7 +2,17 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
 //import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.service.ProductService;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -12,12 +22,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet(urlPatterns = {"/cart"})
 public class CartServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,5 +36,21 @@ public class CartServlet extends HttpServlet {
         context.setVariable("total", Cart.calculateTotalPrice());
         context.setVariable("currency", Cart.getDefaultCurrency());
         engine.process("product/cart.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore, productCategoryDataStore, supplierDataStore);
+
+        String body = req.getReader().readLine();
+        Gson gson = new Gson();
+        HashMap<String,Integer> productIdMap = gson.fromJson(body,new TypeToken<HashMap<String,Integer>>(){}.getType());
+        int productId = productIdMap.get("productId");
+        Product product = productService.getProduct(productId);
+        Cart.add(product);
+
     }
 }
