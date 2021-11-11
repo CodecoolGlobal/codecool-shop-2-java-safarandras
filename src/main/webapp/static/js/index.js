231fa1900a459f5199e7f09e7e56e205a37d44ef
Gezow <kokenyesdanone@gmail.com>
2021-11-11 14:02:17 +0100

    error handling post request and ui update when items added to cart

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,13 +1,27 @@
 function addToCartBtnHandler(element) {
     let productId = element.dataset.prodid;
-    const data = {"productId" : productId};
-    fetch(`/cart`,{
+    const data = {"productId": productId};
+
+    fetch(`/cart`, {
         method: 'POST',
         headers: {
+            'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body:JSON.stringify(data)
-    } );
-    let cartNumber = document.querySelector("#lblCartCount");
-    cartNumber.textContent = (parseInt(cartNumber.textContent) + 1).toString();
+        body: JSON.stringify(data),
+    }).then(function (response) {                      // first then()
+        if (response.ok) {
+            return response.text();
+        }
+
+        throw new Error('Something went wrong.');
+    })
+        .then(function (text) {                          // second then()
+            console.log('Request successful', text);
+            let cartNumber = document.querySelector("#lblCartCount");
+            cartNumber.textContent = (parseInt(cartNumber.textContent) + 1).toString();
+        })
+        .catch(function (error) {                        // catch
+            console.log('Request failed', error);
+        });
 }
