6abbdac148c824d2bf035cac3f78eb28d81fdbd7
Gezow <kokenyesdanone@gmail.com>
2021-11-24 11:56:55 +0100

    CartDaoMem logger

--- src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -1,8 +1,11 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.List;
@@ -11,6 +14,7 @@ public class CartDaoMem implements CartDao {
 
     HashSet<Cart> carts = new HashSet<>();
     private static CartDaoMem instance = null;
+    private static final Logger logger = LoggerFactory.getLogger(CartDaoMem.class);
 
     /* A private Constructor prevents any other class from instantiating.
      */
@@ -26,11 +30,13 @@ public class CartDaoMem implements CartDao {
 
     @Override
     public void add(Cart cart) {
+        logger.debug("Add Cart called");
         carts.add(cart);
     }
 
     @Override
     public Cart find(int cartId) {
+        logger.debug("Find Cart called");
         return carts.stream()
                 .filter(cart -> cartId == cart.getCartId())
                 .findFirst()
@@ -39,11 +45,13 @@ public class CartDaoMem implements CartDao {
 
     @Override
     public void remove(int cartId) {
+        logger.debug("Remove Cart called");
         carts.removeIf(cart -> cartId == cart.getCartId());
     }
 
     @Override
     public HashSet<Cart> getAll() {
+        logger.debug("Get all Cart called");
         return carts;
     }
 
