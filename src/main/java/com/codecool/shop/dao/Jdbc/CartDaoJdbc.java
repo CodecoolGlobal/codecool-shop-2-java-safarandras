3d907a89ec5492083549b54bb486db0e287f17a0
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-11-25 13:49:45 +0100

    feat: add CartDaoJdbc class

---
+++ src/main/java/com/codecool/shop/dao/Jdbc/CartDaoJdbc.java
@@ -0,0 +1,128 @@
+package com.codecool.shop.dao.Jdbc;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.LineItem;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import javax.sql.DataSource;
+import java.lang.reflect.Type;
+import java.sql.*;
+import java.util.HashSet;
+
+public class CartDaoJdbc implements CartDao {
+
+    private DataSource dataSource;
+
+    public CartDaoJdbc(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void add(Cart cart) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "INSERT INTO carts(id, date, userID, items) VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            java.util.Date utilDate = new java.util.Date();
+            Date savedAt = new java.sql.Date(utilDate.getTime());
+
+            String items = new Gson().toJson(cart.getAllLineItem());
+
+            statement.setInt(1, cart.getCartId());
+            statement.setDate(2, savedAt);
+            statement.setInt(3, cart.getUserId());
+            statement.setString(4, items);
+            statement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void update(Cart cart) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "UPDATE carts SET date = ?, items = ? WHERE id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+
+            java.util.Date utilDate = new java.util.Date();
+            Date savedAt = new java.sql.Date(utilDate.getTime());
+
+            String items = new Gson().toJson(cart.getAllLineItem());
+
+            st.setDate(1, savedAt);
+            st.setString(2, items);
+            st.setInt(3, cart.getCartId());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Cart find(int cartId) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT * FROM carts WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, cartId);
+            ResultSet resultSet = statement.executeQuery();
+            if(!resultSet.next()) {
+                return null;
+            }
+
+            Cart cart = new Cart();
+            cart.setCartId(resultSet.getInt(1));
+            cart.setUserId(resultSet.getInt(2));
+            String jsonString = resultSet.getString(3);
+            Type setType = new TypeToken<HashSet<String>>(){}.getType();
+            Gson gson = new Gson();
+            HashSet<LineItem> items = gson.fromJson(jsonString, setType);
+            cart.setLineItems(items);
+            return cart;
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void remove(int cartId) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "DELETE FROM carts WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, cartId);
+            statement.executeUpdate();
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public HashSet<Cart> getAll(int userId) {
+        try(Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT * FROM productcategories";
+            ResultSet resultSet = connection.createStatement().executeQuery(sql);
+
+            HashSet<Cart> carts = new HashSet<>();
+            while(resultSet.next()){
+                Cart cart = new Cart();
+                cart.setCartId(resultSet.getInt(1));
+                cart.setUserId(resultSet.getInt(2));
+                String jsonString = resultSet.getString(3);
+                Type setType = new TypeToken<HashSet<String>>(){}.getType();
+                Gson gson = new Gson();
+                HashSet<LineItem> items = gson.fromJson(jsonString, setType);
+                cart.setLineItems(items);
+                carts.add(cart);
+            }
+            return carts;
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+}
