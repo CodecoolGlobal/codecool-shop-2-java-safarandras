68cc80b2d953c45deee2e1013946bcd1fb6260a0
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-11-26 01:29:33 +0100

    feat: add CartService switch between memory and db to DaoSelector class

--- src/main/java/com/codecool/shop/util/DaoSelector.java
+++ src/main/java/com/codecool/shop/util/DaoSelector.java
@@ -1,14 +1,18 @@
 package com.codecool.shop.util;
 
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.Jdbc.CartDaoJdbc;
 import com.codecool.shop.dao.Jdbc.ProductCategoryJdbc;
 import com.codecool.shop.dao.Jdbc.ProductJdbc;
 import com.codecool.shop.dao.Jdbc.SupplierJdbc;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.memory.CartDaoMem;
 import com.codecool.shop.dao.memory.ProductCategoryDaoMem;
 import com.codecool.shop.dao.memory.ProductDaoMem;
 import com.codecool.shop.dao.memory.SupplierDaoMem;
+import com.codecool.shop.service.CartService;
 import com.codecool.shop.service.ProductService;
 import org.postgresql.ds.PGSimpleDataSource;
 
@@ -23,6 +27,7 @@ public class DaoSelector {
     private static ProductCategoryDao ProductCategoryDao;
     private static ProductDao ProductDao;
     private static SupplierDao SupplierDao;
+    private static CartDao CartDao;
     private static Properties properties = new Properties();
 
 
@@ -45,6 +50,23 @@ public class DaoSelector {
             SupplierDao = SupplierDaoMem.getInstance();
         }
         return new ProductService(ProductDao, ProductCategoryDao, SupplierDao);
+    }
+
+    public static CartService getCartService() throws IOException {
+        properties.load(new FileInputStream("src/main/resources/connection.properties"));
+        if(properties.getProperty("dao").equals("jdbc")){
+            try {
+                DataSource dataSource = connect();
+                CartDao = new CartDaoJdbc(dataSource);
+
+            } catch (SQLException e) {
+                System.err.println("Database connection unavailable!");
+            }
+        }
+        else if (properties.getProperty("dao").equals("memory")) {
+            CartDao = CartDaoMem.getInstance();
+        }
+        return new CartService(CartDao);
     }
 
     public static DataSource connect() throws SQLException {
